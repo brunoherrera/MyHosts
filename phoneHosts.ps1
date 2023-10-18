05edde6e771bf7978f2d9ed24c1df955298e735d
Bruno Herrera <shadypr357@gmail.com>
2023-10-17 22:57:03 -0300

    allow redditmail

--- phoneHosts.ps1
+++ phoneHosts.ps1
@@ -113,6 +113,7 @@ $content = $content | ForEach-Object -Begin { $counter = 0 } -Process {
 	   -replace '0.0.0.0 www.ipify.org','#0.0.0.0 www.ipify.org' `
 	   -replace '0.0.0.0 smartlock.google.com','#0.0.0.0 smartlock.google.com' `
 	   -replace '0.0.0.0 id.google.com.uy','#0.0.0.0 id.google.com.uy' `
+	   -replace '0.0.0.0 click.redditmail.com','#0.0.0.0 click.redditmail.com' `
 	   -replace '0.0.0.0 adx.telegram.com','#0.0.0.0 adx.telegram.com'
 }
 
